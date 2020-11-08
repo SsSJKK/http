aef4f0d9ad909537c97eb1fe7af9156b758551f5
SsS_JKK <kobil07@mail.ru>
2020-11-08 20:59:05 +0500

    1

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"net"
 	"os"
-	"strconv"
 
 	"github.com/SsSJKK/http/pkg/server"
 )
@@ -16,41 +15,14 @@ func main() {
 	if err := execute(host, port); err != nil {
 		os.Exit(1)
 	}
+
 }
 
 func execute(host string, port string) (err error) {
 	srv := server.NewServer(net.JoinHostPort(host, port))
-	srv.Register("/", func(conn net.Conn) {
-		body := "Welcome to our web-site"
-
-		_, err = conn.Write([]byte(
-			"HTTP/1.1 200 OK\r\n" +
-				"Content-Length: " + strconv.Itoa(len(body)) + "r\n" +
-				"Content-Type: application/json\r\n" +
-				"Connection: close\r\n" +
-				"\r\n" +
-				body,
-		))
-		if err != nil {
-			log.Print(err)
-		}
+	srv.Register("/payments", func(req *server.Request) {
+		id := req.QueryParams["id"]
+		log.Print(id)
 	})
-
-	srv.Register("/about", func(conn net.Conn) {
-		body := "About Golang Academy"
-
-		_, err = conn.Write([]byte(
-			"HTTP/1.1 200 OK\r\n" +
-				"Content-Length: " + strconv.Itoa(len(body)) + "r\n" +
-				"Content-Type: text/html\r\n" +
-				"Connection: close\r\n" +
-				"\r\n" +
-				body,
-		))
-		if err != nil {
-			log.Print(err)
-		}
-	})
-
 	return srv.Start()
 }
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -5,43 +5,99 @@ import (
 	"io"
 	"log"
 	"net"
+	"net/url"
 	"strings"
 	"sync"
 )
 
+// ErrBadRequest ...
+const ErrBadRequest = "Error bad request !!!"
+
+// Request ...
+type Request struct {
+	Conn        net.Conn
+	QueryParams url.Values
+}
+
 // HandlerFunc ...
-type HandlerFunc func(conn net.Conn)
+type HandlerFunc func(req *Request)
 
 // Server ...
 type Server struct {
 	addr     string
-	mu       sync.Mutex
+	mu       sync.RWMutex
 	handlers map[string]HandlerFunc
 }
 
-//NewServer ...
+// NewServer ...
 func NewServer(addr string) *Server {
 	return &Server{addr: addr, handlers: make(map[string]HandlerFunc)}
 }
 
-//Register ...
+// Register ...
 func (s *Server) Register(path string, handler HandlerFunc) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	s.handlers[path] = handler
 }
 
-//Start ...
-func (s *Server) Start() error {
+// handle ...
+func (s *Server) handle(conn net.Conn) {
+	defer func() {
+		if cerr := conn.Close(); cerr != nil {
+			log.Println(cerr)
+		}
+	}()
+	for {
+		buf := make([]byte, 4096)
+		n, err := conn.Read(buf)
+		if err == io.EOF {
+			log.Printf("%s", buf[:n])
+		}
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		//log.Printf("%s", buf[:n])
 
-	listner, err := net.Listen("tcp", s.addr)
+		data := buf[:n]
+		rnIndex := []byte{'\r', '\n'}
+		reqEnd := bytes.Index(data, rnIndex)
+		if reqEnd == -1 {
+			log.Println(ErrBadRequest)
+			return
+		}
+		reqLine := string(data[:reqEnd])
+		parts := strings.Split(reqLine, " ")
+		if len(parts) != 3 {
+			log.Println(ErrBadRequest)
+			return
+		}
+		path := parts[1]
+		uri, err := url.ParseRequestURI(path)
+		if err != nil {
+			log.Println("error in decoding")
+			return
+		}
+		var req = Request{conn, uri.Query()}
+		s.mu.RLock()
+		fn, ok := s.handlers[uri.Path]
+		s.mu.RUnlock()
+		if ok {
+			fn(&req)
+		}
+	}
+}
+
+// Start ...
+func (s *Server) Start() error {
+	listener, err := net.Listen("tcp", s.addr)
 	if err != nil {
-		log.Print(err, 1)
+		log.Print(err)
 		return err
 	}
-
 	defer func() {
-		if cerr := listner.Close(); cerr != nil {
+		if cerr := listener.Close(); cerr != nil {
 			if err == nil {
 				err = cerr
 				return
@@ -51,65 +107,11 @@ func (s *Server) Start() error {
 	}()
 
 	for {
-		conn, err := listner.Accept()
+		conn, err := listener.Accept()
 		if err != nil {
 			log.Print(err)
 			continue
 		}
-
 		go s.handle(conn)
-		if err != nil {
-			log.Print(err)
-			continue
-		}
 	}
-}
-
-func (s *Server) handle(conn net.Conn) (err error) {
-	mu := sync.RWMutex{}
-	defer func() {
-		if cerr := conn.Close(); cerr != nil {
-			if err == nil {
-				err = cerr
-				return
-			}
-			log.Print(err)
-		}
-	}()
-	//	conn.Write([]byte("Hello!\r\n"))
-
-	buf := make([]byte, 4096)
-	n, err := conn.Read(buf)
-	if err == io.EOF {
-		log.Printf("%s", buf[:n])
-	}
-	if err != nil {
-		return err
-	}
-	data := buf[:n]
-	requestLineDelim := []byte{'\r', '\n'}
-	requestLineEnd := bytes.Index(data, requestLineDelim)
-	if requestLineEnd == -1 {
-		return
-	}
-	requestLine := string(data[:requestLineEnd])
-	parts := strings.Split(requestLine, " ")
-	if len(parts) != 3 {
-		return
-	}
-	path, version := parts[1], parts[2]
-
-	if version != "HTTP/1.1" {
-		return
-	}
-	mu.RLock()
-	for k, v := range s.handlers {
-		if k == path {
-			v(conn)
-		}
-	}
-	mu.RUnlock()
-
-	return nil
-
 }
